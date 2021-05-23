a9f79e63c82efa4a464422df33ede1736936938a
Audrey Chu <audrey.chiahui.chu@gmail.com>
2021-05-23 18:08:07 +0800

    專案範例

---
+++ Interfaces/ICommentService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelCat2021.Interfaces
+{
+  public interface ICommentService
+  {
+    Task<List<string>> Get();
+  }
+}
---
+++ Services/CommentService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TravelCat2021.Interfaces;
+
+namespace TravelCat2021.Services
+{
+  public class CommentService : ICommentService
+  {
+    public async Task<List<string>> Get()
+    {
+      return new List<string> { "第一則", "第二則" };
+    }
+  }
+}
