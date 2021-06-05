f4dbc956435bde99e5658b622ab98d90fda3f048
Audrey Chu <audrey.chiahui.chu@gmail.com>
2021-06-06 01:59:15 +0800

    gitflow-feature-stash: fakeApis

---
+++ TravelCat2021/Interfaces/IAttractionService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TravelCat2021.ViewModels;
+
+namespace TravelCat2021.Interfaces
+{
+  public interface IAttractionService
+  {
+    Task<AttractionDetailView> Get(int id);
+  }
+}
